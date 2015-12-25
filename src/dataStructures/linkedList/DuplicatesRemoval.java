b61157ec7c6cd5e31f9bf80bd73a35ddaecb9abe
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-12-25 02:23:53 -0600

    Refactored the duplicate removal method.

--- src/dataStructures/linkedList/DuplicatesRemoval.java
+++ src/dataStructures/linkedList/DuplicatesRemoval.java
@@ -9,17 +9,16 @@ package dataStructures.linkedList;
  * 
  * How would you solve this problem if a temporary buffer is not allowed?
  * 
- * Questions that came to my mind:
- * 	- is white spaces ' ' significant, do I need to remove that?
- *  - is case significant? How about a and A.
+ * Questions that came to my mind: - is white spaces ' ' significant, do I need
+ * to remove that? - is case significant? How about a and A.
  * 
  * @author Minh
  *
  */
 public class DuplicatesRemoval {
-	
+
 	public static void main(String[] args) {
-		
+
 		// Test the list
 		System.out.println("List test");
 		DuplicatesRemoval removal = new DuplicatesRemoval();
@@ -28,67 +27,70 @@ public class DuplicatesRemoval {
 		list.add("B");
 		list.add("A");
 		System.out.println(list);
-		
+
 		list.add("A");
 		list.add("0");
 		list.add("-1");
 		System.out.println(list);
-		
+
 		// Test the remove
 		System.out.println("Removing duplicates from FOLLOW UP");
 		removal.remove("FOLLOW UP");
 	}
-	
+
+	/**
+	 * This algorithm takes O(n) times to convert the input into a linked list.
+	 * And, it takes also another n(n-1) + (n-1)(n-2) + (n-2)(n-3) ... to delete
+	 * the duplicates.
+	 * 
+	 * @param input
+	 * @return
+	 */
 	public String remove(String input) {
-		
+
 		// convert the provided input into a linked list
 
 		char[] charArray = input.toCharArray();
 		LinkedList list = new LinkedList();
-		for (int i = charArray.length-1; i >= 0; i--) {
+		for (int i = charArray.length - 1; i >= 0; i--) {
 			list.add(String.valueOf(charArray[i]));
 		}
-		
-		System.out.println(list);
-		
+
 		// remove duplicates
 		WordNode pointer = list.root;
 		WordNode runner = pointer.next;
 		WordNode previous = pointer;
-		
-		while (pointer != null){
-			
+
+		while (pointer != null) {
+
 			while (runner != null) {
 				if (runner.data.equals(pointer.data)) {
 					previous.next = runner.next;
-				}
-				else {
+				} else {
 					previous = runner;
 				}
 				runner = runner.next;
 			}
-			
+
 			pointer = pointer.next;
 			if (pointer != null)
 				runner = pointer.next;
 			previous = pointer;
 		}
-		
-		System.out.println(list);
-		
-		return null;
+
+		return list.toStringNoSpace();
 	}
 
 	class LinkedList {
-		
+
 		public WordNode root;
 		public int size;
-		
+
 		public LinkedList() {
 			root = null;
 			size = 0;
 		}
-		
+
 		public void add(String data) {
 			if (isEmpty())
 				root = new WordNode(data);
@@ -99,33 +101,45 @@ public class DuplicatesRemoval {
 			}
 			size++;
 		}
-		
+
 		public boolean isEmpty() {
 			return (size == 0);
 		}
-		
+
 		public String toString() {
 			StringBuilder sb = new StringBuilder();
-			
+
 			WordNode runner = root;
 			while (runner != null) {
 				sb.append(runner.data + " ");
 				runner = runner.next;
 			}
-			
+
 			return sb.toString();
 		}
-		
+
+		public String toStringNoSpace() {
+			StringBuilder sb = new StringBuilder();
+
+			WordNode runner = root;
+			while (runner != null) {
+				sb.append(runner.data);
+				runner = runner.next;
+			}
+
+			return sb.toString();
+		}
+
 	}
-	
+
 	class WordNode {
-		
+
 		public String data;
 		public WordNode next;
-		
+
 		public WordNode(String data) {
 			this.data = data;
 		}
 	}
-	
+
 }
