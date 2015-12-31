1d4b829103f147048ab438930dbb14faf7a39a9e
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-12-31 00:20:42 -0600

    Implemented basic linked list with root and tail and basic String node.

---
+++ src/dataStructures/linkedList/BasicLinkedList.java
@@ -0,0 +1,87 @@
+package dataStructures.linkedList;
+
+public class BasicLinkedList {
+	
+	public BasicNode root;
+	public BasicNode tail;
+	public int size;
+
+	public BasicLinkedList() { }
+
+	public void addStart(String data) {
+		BasicNode node = new BasicNode(data);
+		if (isEmpty()) {
+			root = node;
+			tail = node;
+		}
+		else {
+			node.next = root;
+			root = node;
+		}
+		size++;
+	}
+	
+	public void addStart(BasicNode node) {
+		if (isEmpty()) {
+			root = node;
+			tail = node;
+		}
+		else {
+			node.next = root;
+			root = node;
+		}
+		size++;
+	}
+	
+	public void addEnd(String data) {
+		BasicNode node = new BasicNode(data);
+		if (isEmpty()) {
+			root = node;
+			tail = node;
+		}
+		else {
+			tail.next = node;
+			tail = node;
+		}
+		size++;
+	}
+	
+	public void addEnd(BasicNode node) {
+		if (isEmpty()) {
+			root = node;
+			tail = node;
+		}
+		else {
+			tail.next = node;
+			tail = node;
+		}
+		size++;
+	}
+
+	public boolean isEmpty() {
+		return (size == 0);
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		BasicNode runner = root;
+		while (runner != null) {
+			sb.append(runner.data + " ");
+			runner = runner.next;
+		}
+
+		return sb.toString();
+	}
+	
+	public BasicNode getNode(String index) {
+		
+		BasicNode runner = root;
+		while (runner != null && !runner.data.equals(index)) {
+			runner = runner.next;
+		}
+		
+		return runner;
+	}
+
+}
---
+++ src/dataStructures/linkedList/BasicNode.java
@@ -0,0 +1,11 @@
+package dataStructures.linkedList;
+
+public class BasicNode {
+
+	String data;
+	BasicNode next;
+
+	public BasicNode(String data) {
+		this.data = data;
+	}
+}
