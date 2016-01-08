2811f763ee97aacc69f0fde68b4e6d4de8944d0a
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-01-08 15:20:02 -0600

    Tested the push method. Fixed a few bugs (mixing stack index)

--- src/dataStructures/stacksAndQueues/ArrayForThreeStacks.java
+++ src/dataStructures/stacksAndQueues/ArrayForThreeStacks.java
@@ -15,6 +15,67 @@ public class ArrayForThreeStacks {
 	int[] pointers;
 	String[] content;
 
+	public static void main(String[] args) {
+		
+		System.out.println("Check for pushing then peeking immediately");
+		ArrayForThreeStacks stacks = new ArrayForThreeStacks();
+		stacks.push(1, "Minh11");
+		System.out.println(stacks.peek(1));
+		stacks.push(1, "Minh12");
+		System.out.println(stacks.peek(1));
+		stacks.push(2, "Minh21");
+		System.out.println(stacks.peek(2));
+		stacks.push(2, "Minh22");
+		System.out.println(stacks.peek(2));
+		stacks.push(3, "Minh31");
+		System.out.println(stacks.peek(3));
+		stacks.push(3, "Minh32");
+		System.out.println(stacks.peek(3));
+		System.out.println();
+		
+		System.out.println("Check for auto expanding");
+		ArrayForThreeStacks smallStacks = new ArrayForThreeStacks(3);
+		smallStacks.push(1, "Minh1");
+		smallStacks.push(1, "Minh2");
+		smallStacks.push(1, "Minh3");
+		smallStacks.push(1, "Minh4");
+		smallStacks.push(1, "Minh5");
+		smallStacks.push(1, "Minh6");
+		smallStacks.push(1, "Minh7");
+		smallStacks.push(1, "Minh8");
+		smallStacks.push(1, "Minh9");
+		smallStacks.push(1, "Minh10");
+		smallStacks.push(1, "Minh11");
+		smallStacks.push(1, "Minh12");
+		smallStacks.push(1, "Minh13");
+		smallStacks.push(1, "Minh14");
+		smallStacks.push(1, "Minh15");
+		smallStacks.push(1, "Minh16");
+		System.out.println(smallStacks.peek(1));
+		
+		smallStacks.push(2, "Minh1");
+		smallStacks.push(2, "Minh2");
+		smallStacks.push(2, "Minh3");
+		smallStacks.push(2, "Minh4");
+		smallStacks.push(2, "Minh5");
+		smallStacks.push(2, "Minh6");
+		System.out.println(smallStacks.peek(2));
+		
+		smallStacks.push(3, "Minh1");
+		smallStacks.push(3, "Minh2");
+		smallStacks.push(3, "Minh3");
+		smallStacks.push(3, "Minh4");
+		smallStacks.push(3, "Minh5");
+		smallStacks.push(3, "Minh6");
+		smallStacks.push(3, "Minh7");
+		smallStacks.push(3, "Minh8");
+		System.out.println(smallStacks.peek(3));
+		
+		System.out.println(smallStacks.peek(1));
+		System.out.println(smallStacks.peek(2));
+		System.out.println(smallStacks.peek(3));
+	}
+	
 	public ArrayForThreeStacks(int defaultSize) {
 		maxSizes = new int[3];
 		currentSizes = new int[3];
@@ -29,17 +90,24 @@ public class ArrayForThreeStacks {
 	public ArrayForThreeStacks() {
 		this(10);
 	}
+	
+	public String peek(int stackNum) {
+		int stackIndex = stackNum-1;
+		return content[pointers[stackIndex]-1];
+	}
 
 	public void push(int stackNum, String item) {
+		int stackIndex = stackNum-1;
 		if (!isValidStackNum(stackNum))
 			throw new IllegalArgumentException("Provided stack number is not valid. Please enter 1, 2 or 3.");
 		else if (isFull(stackNum)) {
 			expand(stackNum);
 		}
 		// Add the item into the staack
-		content[pointers[stackNum]] = item;
-		pointers[stackNum]++;
-		currentSizes[stackNum]++;
+		
+		content[pointers[stackIndex]] = item;
+		pointers[stackIndex]++;
+		currentSizes[stackIndex]++;
 
 	}
 
@@ -88,8 +156,8 @@ public class ArrayForThreeStacks {
 		content = newContent;
 	}
 
-	private boolean isFull(int stackNum) {
-		return currentSizes[stackNum] == maxSizes[stackNum];
+	private boolean isFull(int stackIndex) {
+		return currentSizes[stackIndex-1] == maxSizes[stackIndex-1];
 	}
 
 	private boolean isValidStackNum(int stackNum) {
