1a5a174aba075389061701de453f734377bdde44
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-01-30 01:57:21 -0600

    Set function for bit manipulation

--- src/dataStructures/bitManipulation/BasicBitManipulation.java
+++ src/dataStructures/bitManipulation/BasicBitManipulation.java
@@ -6,7 +6,12 @@ public class BasicBitManipulation {
 		
 		byte a = 0b0001_0100;
 		System.out.println(getBit(a, 4));
-		
+		System.out.println(Integer.toBinaryString(setBit(a, 3)));
+		System.out.println(Byte.toString(setBit(a, 7)));
+	}
+	
+	static byte setBit(byte num, int i) {
+		return (byte) (num | (1 << i));
 	}
 	
 	static boolean getBit(byte num, int i) {
