506b506267ad317445a1e313bb0a8c2ab397d325
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-01-30 02:16:49 -0600

    Added a function to clear the bit at index i

--- src/dataStructures/bitManipulation/BasicBitManipulation.java
+++ src/dataStructures/bitManipulation/BasicBitManipulation.java
@@ -6,16 +6,27 @@ public class BasicBitManipulation {
 		
 		byte a = 0b0001_0100;
 		System.out.println(getBit(a, 4));
-		System.out.println(Integer.toBinaryString(setBit(a, 3)));
-		System.out.println(Byte.toString(setBit(a, 7)));
+		
+		printByte(setBit(a, 3));
+		printByte(setBit(a, 7));
+		printByte(clearBit(a, 2));
+		printByte(clearBit(a, 7));
 	}
 	
 	static byte setBit(byte num, int i) {
 		return (byte) (num | (1 << i));
 	}
 	
+	static byte clearBit(byte num, int i) {
+		byte mask = (byte) ~(1 << i);
+		return (byte) (num & mask);
+	}
+	
 	static boolean getBit(byte num, int i) {
 		return (num & (1 << i)) != 0;
 	}
 
+	static void printByte(byte num) {
+		System.out.println(Integer.toBinaryString(num & 0xFF));
+	}
 }
