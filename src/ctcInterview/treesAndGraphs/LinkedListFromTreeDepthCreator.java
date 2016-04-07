4e883f8abc185f063a5fa7589cd11f016b3a5cdc
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-04-07 00:54:27 -0500

    Finished making list of nodes according to tree's depth. (Level-order)

---
+++ src/ctcInterview/treesAndGraphs/LinkedListFromTreeDepthCreator.java
@@ -0,0 +1,61 @@
+package ctcInterview.treesAndGraphs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * Given a binary tree, design an algorithm which creates a linked 
+ * list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists)
+ */
+public class LinkedListFromTreeDepthCreator {
+	
+	public static void main(String[] args) {
+		
+		int[] in1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		BalancedTreeCreator creator = new BalancedTreeCreator();
+		Tree tree = creator.createBalancedTree(in1);
+		printDebug(create(tree));
+		
+	}
+
+	public static LinkedList<LinkedList<Node>> create(Tree tree) {
+		
+		Queue<Node> listOfNodesToExploreInCurrentOrder = new LinkedList<>();
+		Queue<Node> listOfNodesToExploreInNextOrder = new LinkedList<>();
+		LinkedList<LinkedList<Node>> set = new LinkedList<>();
+		
+		listOfNodesToExploreInCurrentOrder.add(tree.root);
+		
+		while (!listOfNodesToExploreInCurrentOrder.isEmpty()) {
+			
+			LinkedList<Node> listCurrentLevel = new LinkedList<>();
+			
+			while (!listOfNodesToExploreInCurrentOrder.isEmpty()) {
+				Node node = listOfNodesToExploreInCurrentOrder.remove();
+				listCurrentLevel.add(node);
+				
+				if (node.left != null)
+					listOfNodesToExploreInNextOrder.add(node.left);
+				if (node.right != null)
+					listOfNodesToExploreInNextOrder.add(node.right);
+			}
+			
+			set.add(listCurrentLevel);
+			
+			listOfNodesToExploreInCurrentOrder = listOfNodesToExploreInNextOrder;
+			listOfNodesToExploreInNextOrder = new LinkedList<>();
+		}
+
+		return set;
+	}
+
+	public static void printDebug(LinkedList<LinkedList<Node>> set) {
+		for (LinkedList<Node> linkedList : set) {
+			for (Node node : linkedList) {
+				System.out.printf("%d ", node.id);
+			}
+			System.out.println();
+		}
+	}
+	
+}
