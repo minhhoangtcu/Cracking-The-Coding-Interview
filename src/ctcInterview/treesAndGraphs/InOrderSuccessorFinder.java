34d2964a97d5aba5f970e2c610ebb9dd5b422557
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-04-09 14:38:54 -0500

    Added comment

--- src/ctcInterview/treesAndGraphs/InOrderSuccessorFinder.java
+++ src/ctcInterview/treesAndGraphs/InOrderSuccessorFinder.java
@@ -1,8 +1,9 @@
 package ctcInterview.treesAndGraphs;
 
-/*
- * Write an algorithm to find the 'next' node (i.e., in-order successor) of a given node in 
- * a binary search tree. You may assume that each node has a link to its parent.
+/**
+ * Write an algorithm to find the 'next' node (i.e., in-order successor) of a
+ * given node in a binary search tree. You may assume that each node has a link
+ * to its parent.
  */
 public class InOrderSuccessorFinder {
 
