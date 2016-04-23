470fae5676ce2bce1681d74c298468ca1990ca4c
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-04-23 00:18:38 -0500

    Create Balanced tree by going through nodes.

--- src/ctcInterview/treesAndGraphs/BalancedTreeCreator.java
+++ src/ctcInterview/treesAndGraphs/BalancedTreeCreator.java
@@ -15,6 +15,32 @@ public class BalancedTreeCreator {
 		int[] in2 = {1, 2, 3, 4, 5, 6, 7};
 		creator.createBalancedTree(in2);
 	}
+	
+	public Tree createBalancedTreeNode(int[] elements) {
+		
+		Tree tree = new Tree(true);
+		
+		int start = 0;
+		int end = elements.length-1;
+		
+		addNode(tree.root, elements, start, end);
+		
+		return tree;
+		
+	}
+	
+	public Node addNode(Node node, int[] elements, int start, int end) {
+		
+		if (start > end)
+			return null;
+		
+		int mid = (start + end)/2;
+		
+		node.left = addNode(node.left, elements, start, mid-1);
+		node.right = addNode(node.right, elements, start, mid+1);
+		
+		return node;
+	}
 
 	/*
 	 * Inefficient because calling tree.add method would make the tree go down searching for
