2b2449e41d2874c3811719f6669eb23d7069b41e
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-04-28 23:24:39 -0500

    Finished moving robot dynamically

--- src/ctcInterview/recursionAndDynamicProgramming/RD92_Robot.java
+++ src/ctcInterview/recursionAndDynamicProgramming/RD92_Robot.java
@@ -15,11 +15,47 @@ package ctcInterview.recursionAndDynamicProgramming;
  *
  */
 public class RD92_Robot {
-	
+
 	public static void main(String[] args) {
+
+		int x, y;
+		Timer t = new Timer();
 		
-		System.out.println(countNumberOfPaths(0, 0, 2, 2));
+		t.start();
+		System.out.println(countNumberOfPaths(0, 0, x=10, y=10));
+		t.stopAndPrint();
 		
+		int[][] paths = new int[x][y];
+		t.start();
+		System.out.println(countNumberOfPathsDynamically(paths, 0, 0, x=10, y=10));
+		t.stopAndPrint();
+		
+		paths = new int[x=20][x=20];
+		t.start();
+		System.out.println(countNumberOfPathsDynamically(paths, 0, 0, x, y));
+		t.stopAndPrint();
+	}
+
+	public static int countNumberOfPathsDynamically(int[][] computedPaths, int currentX, int currentY, int x, int y) {
+
+		// Computed position
+		if (computedPaths[currentX][currentY] != 0)
+			return computedPaths[currentX][currentY];
+
+		// Found the position, returning 1 path. Or if it reaches the end of row
+		// of the end of a col, then there is only 1 posible way
+		if (currentX == x || currentY == y)
+			return 1;
+
+		// Because we cannot go to left, if current X > X, then there is no way
+		// after this one. Same for going up with Y.
+		if (currentX > x || currentY > y)
+			return 0;
+
+		computedPaths[currentX][currentY] = countNumberOfPaths(currentX + 1, currentY, x, y)
+				+ countNumberOfPaths(currentX, currentY + 1, x, y);
+
+		return computedPaths[currentX][currentY];
 	}
 
 	public static int countNumberOfPaths(int currentX, int currentY, int x, int y) {
@@ -33,9 +69,9 @@ public class RD92_Robot {
 		if (currentX > x || currentY > y)
 			return 0;
 
-		int numberOfPaths = countNumberOfPaths(currentX+1, currentY, x, y) + countNumberOfPaths(currentX, currentY+1, x, y);
-		
+		int numberOfPaths = countNumberOfPaths(currentX + 1, currentY, x, y)
+				+ countNumberOfPaths(currentX, currentY + 1, x, y);
+
 		return numberOfPaths;
 	}
-
 }
