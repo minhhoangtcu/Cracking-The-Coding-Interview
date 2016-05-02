3208d121c3e8989a12aeecbbdefc0b3410a5967f
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-05-01 21:52:10 -0500

    Completed find path (found bug in compute all for offlimit)

--- src/ctcInterview/recursionAndDynamicProgramming/RD92_Robot.java
+++ src/ctcInterview/recursionAndDynamicProgramming/RD92_Robot.java
@@ -1,5 +1,7 @@
 package ctcInterview.recursionAndDynamicProgramming;
 
+import java.util.LinkedList;
+
 /**
  * Imagine a robot sitting on the upper left corner of an X by Y grid. The robot
  * can only move in two directions: right and down. How many possible paths are
@@ -22,14 +24,14 @@ public class RD92_Robot {
 		int x, y;
 		Timer t = new Timer();
 
-		t.start();
-		System.out.println(countNumberOfPaths(0, 0, x = 10, y = 10));
-		t.stopAndPrint();
-
-		int[][] paths = new int[x = 10][y = 10];
-		t.start();
-		System.out.println(countNumberOfPathsDynamically(paths, 0, 0, x - 1, y - 1));
-		t.stopAndPrint();
+//		t.start();
+//		System.out.println(countNumberOfPaths(0, 0, x = 10, y = 10));
+//		t.stopAndPrint();
+//
+//		int[][] paths = new int[x = 10][y = 10];
+//		t.start();
+//		System.out.println(countNumberOfPathsDynamically(paths, 0, 0, x - 1, y - 1));
+//		t.stopAndPrint();
 
 		// Time consuming version
 		// paths = new int[x=20][y=20];
@@ -39,15 +41,85 @@ public class RD92_Robot {
 		// t.stopAndPrint();
 
 		// FOLLOW UP VERSION
-		paths = new int[x = 3][y = 3];
-		paths[1][1] = -1; // off-limit
-		System.out.println(countNumberOfPathsDynamically(paths, 0, 0, x - 1, y - 1));
+		System.out.println("<<< FIND PATH 3x3 >>>");
+		int[][] map = new int[x = 3][y = 3];
+		map[1][1] = OFFLIMIT; // off-limit
+//		System.out.println(countNumberOfPathsDynamically(paths, 0, 0, x - 1, y - 1));
+		
+		RD92_Robot robot = new RD92_Robot();
+		
+		LinkedList<Position> path = robot.findPath(map, x-1, y-1);
+		
+		if (path != null) {
+			for (Position position : path) {
+				System.out.printf("x: %d\ty: %d\n", position.x, position.y);
+			}
+		}
+		
+		System.out.println("<<< FIND PATH 5x4 >>>");
+		map = new int[x=5][y=4];
+		map[2][0] = OFFLIMIT;
+		map[2][1] = OFFLIMIT;
+		map[4][2] = OFFLIMIT;
+		
+		path = robot.findPath(map, x-1, y-1);
+		
+		if (path != null) {
+			for (Position position : path) {
+				System.out.printf("x: %d\ty: %d\n", position.x, position.y);
+			}
+		}
+		
+	}
+
+	/**
+	 * Find a path for the rebot from the top left to the bottom right.
+	 * 
+	 * @param map
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public LinkedList<Position> findPath(int[][] map, int x, int y) {
+		LinkedList<Position> path = new LinkedList<>();
+		findPathHelper(map, 0, 0, x, y, path);
+		return path;
+	}
+
+	private static int VISITED = 1;
+	private static int OFFLIMIT = -1;
+
+	private boolean findPathHelper(int[][] map, int currentX, int currentY, int x, int y,
+			LinkedList<Position> path) {
+
+		// got to the position
+		if (currentX == x && currentY == y) {
+			path.add(new Position(currentX, currentY));
+			return true;
+		}
+			
+
+		// off range
+		if (currentX > x || currentY > y)
+			return false;
+
+		// Go either right or down, if from here we can reach the end, add the position to path.
+		if (currentX+1 <= x && map[currentX + 1][currentY] != OFFLIMIT && map[currentX + 1][currentY] != VISITED
+				&& findPathHelper(map, currentX + 1, currentY, x, y, path)) {
+			path.add(new Position(currentX, currentY));
+		} else if (currentY <= y && map[currentX][currentY + 1] != OFFLIMIT && map[currentX][currentY + 1] != VISITED
+				&& findPathHelper(map, currentX, currentY + 1, x, y, path)) {
+			path.add(new Position(currentX, currentY));
+		}
+
+		return !path.isEmpty();
+		
 	}
 
 	/**
 	 * Count the number of paths from (currentX, currentY) to (x, y). Also,
 	 * assign the number of paths from to the array for each (currentX,
-	 * currentY). 
+	 * currentY).
 	 * 
 	 * @param computedPaths
 	 * @param currentX
@@ -64,7 +136,8 @@ public class RD92_Robot {
 
 		// Found the position, returning 1 path. Or if it reaches the end of row
 		// of the end of a col, then there is only 1 possible way
-		if (currentX == x || currentY == y)
+
+		if (currentX == x && currentY == y)
 			return 1;
 
 		// Because we cannot go to left, if current X > X, then there is no way
@@ -72,10 +145,12 @@ public class RD92_Robot {
 		if (currentX > x || currentY > y)
 			return 0;
 
-		if (computedPaths[currentX + 1][currentY] != -1)
-			computedPaths[currentX][currentY] += countNumberOfPathsDynamically(computedPaths, currentX + 1, currentY, x, y);
-		if (computedPaths[currentX][currentY + 1] != -1)
-			computedPaths[currentX][currentY] += countNumberOfPathsDynamically(computedPaths, currentX, currentY + 1, x, y);
+		if (computedPaths[currentX + 1][currentY] != OFFLIMIT)
+			computedPaths[currentX][currentY] += countNumberOfPathsDynamically(computedPaths, currentX + 1, currentY, x,
+					y);
+		if (computedPaths[currentX][currentY + 1] != OFFLIMIT)
+			computedPaths[currentX][currentY] += countNumberOfPathsDynamically(computedPaths, currentX, currentY + 1, x,
+					y);
 
 		return computedPaths[currentX][currentY];
 	}
@@ -95,5 +170,16 @@ public class RD92_Robot {
 				+ countNumberOfPaths(currentX, currentY + 1, x, y);
 
 		return numberOfPaths;
+	}
+
+	class Position {
+		int x;
+		int y;
+
+		public Position(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
 	}
 }
