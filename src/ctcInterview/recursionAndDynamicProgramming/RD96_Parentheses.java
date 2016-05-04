35c1b50448250f35f1baa86df643cb7fbaff6b72
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-05-04 13:09:45 -0500

    Added comment and TODO for more efficient algorithm.

--- src/ctcInterview/recursionAndDynamicProgramming/RD96_Parentheses.java
+++ src/ctcInterview/recursionAndDynamicProgramming/RD96_Parentheses.java
@@ -17,53 +17,62 @@ import java.util.Set;
  *
  */
 public class RD96_Parentheses {
-	
+
 	public static void main(String[] args) {
-		
+
 		RD96_Parentheses p = new RD96_Parentheses();
-		
-		for (String combi: p.getCombinations(4)) {
+
+		for (String combi : p.getCombinations(4)) {
 			System.out.println(combi);
 		}
-		
+
 	}
 
 	/**
+	 * Recursively build combinations of parentheses using an array. Each
+	 * recursive call will put either left or right parenthesis at an index of
+	 * the mentioned array. This way, we can prevent computation time for
+	 * duplicate strings.
+	 * 
+	 */
+	//TODO: Implement the algorithm.
+
+	/**
 	 * <p>
-	 * Base case: ()<br> 
-	 * For n = 2: ()(), (())<br> 
+	 * Base case: ()<br>
+	 * For n = 2: ()(), (())<br>
 	 * For n = 3: ()()(), (())(), ()(()), (()()), ((()))
 	 * </p>
 	 * 
 	 * <p>
-	 * For case n, we add () to each position within a String s in
-	 * case n-1. Repeating this process ignoring what we already generated will
-	 * give us all valid parentheses.
+	 * For case n, we add () to each position within a String s in case n-1.
+	 * Repeating this process ignoring what we already generated will give us
+	 * all valid parentheses.
 	 * </p>
 	 * 
 	 */
 	public Set<String> getCombinations(int n) {
-		
+
 		Set<String> result = new HashSet<>();
-		
+
 		if (n < 1)
 			return null;
 		else if (n == 1) {
 			result.add("()");
 			return result;
 		}
-		
-		for (String smaller: getCombinations(n-1)) {
-			
+
+		for (String smaller : getCombinations(n - 1)) {
+
 			for (int i = 0; i < smaller.length(); i++) {
-				
+
 				String newCombi = smaller.substring(0, i) + "()" + smaller.substring(i);
 				if (!result.contains(newCombi))
 					result.add(newCombi);
-				
+
 			}
 		}
-		
+
 		return result;
 	}
 }
