c5637d03b579cadf48c564903751b45056828416
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-05-06 09:25:23 -0500

    Finished image painting

--- src/ctcInterview/arraysAndString/MatrixPrinter.java
+++ src/ctcInterview/arraysAndString/MatrixPrinter.java
@@ -3,9 +3,10 @@ package ctcInterview.arraysAndString;
 public class MatrixPrinter {
 	
 	public static void print(int[][] matrix) {
+		
 		for (int row = 0; row < matrix[0].length; row++) {
 			
-			for (int col = 0; col < matrix[0].length; col++) {
+			for (int col = 0; col < matrix.length; col++) {
 				System.out.printf("%2d ", matrix[col][row]);
 			}
 			System.out.println();
