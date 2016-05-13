30ce80a14e532e49f1d31cf1873ff6d13bb5d117
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-05-13 08:58:40 +0700

    Improved the circus algorithm with dynamic programming.

--- src/ctcInterview/sortingAndSearching/SS1007_Circus.java
+++ src/ctcInterview/sortingAndSearching/SS1007_Circus.java
@@ -1,5 +1,7 @@
 package ctcInterview.sortingAndSearching;
 
+import java.util.Hashtable;
+
 /**
  * A circus is designing a tower routine consisting of people standing atop one
  * another's shoulders. For practical and aesthetic reasons, each person must be
@@ -19,65 +21,81 @@ package ctcInterview.sortingAndSearching;
  *
  */
 public class SS1007_Circus {
-	
+
 	public static void main(String[] args) {
-		
+
 		SS1007_Circus c = new SS1007_Circus();
-		
+
 		Person p1 = c.new Person(65, 100);
 		Person p2 = c.new Person(70, 150);
 		Person p3 = c.new Person(56, 90);
 		Person p4 = c.new Person(75, 190);
 		Person p5 = c.new Person(60, 95);
 		Person p6 = c.new Person(68, 110);
-		
-		Person[] people1 = {p1, p3};
-		Person[] people2 = {p3, p1};
-		Person[] people3 = {p1, p2, p3, p4, p5, p6};
-		
+		Person p7 = c.new Person(50, 120);
+
+		Person[] people1 = { p1, p3 };
+		Person[] people2 = { p3, p1 };
+		Person[] people3 = { p1, p2, p3, p4, p5, p6 };
+		Person[] people4 = { p7, p6 };
+		Person[] people5 = { p1, p2, p3, p4, p5, p6, p7 };
+		Person[] people6 = { p1, p2, p3, p4, p5, p7 };
+
 		System.out.println(c.getLargest(people1));
 		System.out.println(c.getLargest(people2));
 		System.out.println(c.getLargest(people3));
-		
+		System.out.println(c.getLargest(people4));
+		System.out.println(c.getLargest(people5));
+		System.out.println(c.getLargest(people6));
+
 	}
-	
+
 	public int getLargest(Person[] people) {
 		int max = Integer.MIN_VALUE;
-		for (Person person: people) {
-			int largestWithPersonAsBase = getLargestHelper(people, person);
+		for (Person person : people) {
+			int largestWithPersonAsBase = getLargestHelper(people, person, new Hashtable<>());
 			if (largestWithPersonAsBase > max)
 				max = largestWithPersonAsBase;
 		}
 		return max;
 	}
-	
-	private int getLargestHelper(Person[] people, Person base) {
-		
+
+	private int getLargestHelper(Person[] people, Person base, Hashtable<Person, Integer> maxWithBase) {
+
+		if (maxWithBase.containsKey(base))
+			return maxWithBase.get(base);
+
 		int max = Integer.MIN_VALUE;
 		boolean isTop = true;
-		
-		for (Person person: people) {
-			if (person != base && person.compareTo(base) < 0) { // Try and stack everyone on the provided base person
+
+		for (Person person : people) {
+			if (person != base && person.compareTo(base) < 0) {
+				// Try and stack everyone on the provided base person
 				isTop = false;
-				int largestWithPersonAsBase = getLargestHelper(people, person);
+				int largestWithPersonAsBase = getLargestHelper(people, person, maxWithBase);
 				if (largestWithPersonAsBase > max)
 					max = largestWithPersonAsBase;
 			}
 		}
-		
-		if (isTop) // If we cannot put any other person on top of base
+
+		if (isTop) { // If we cannot put any other person on top of base
+			maxWithBase.put(base, 1);
 			return 1;
-		else
+		} else {
+			maxWithBase.put(base, max + 1);
 			return max + 1;
+		}
 	}
-	
+
 	public class Person implements Comparable<Person> {
 		int height;
 		int weight;
+
 		public Person(int height, int weight) {
 			this.height = height;
 			this.weight = weight;
 		}
+
 		@Override
 		public int compareTo(Person o) {
 			if (o.height == height && o.weight == weight)
