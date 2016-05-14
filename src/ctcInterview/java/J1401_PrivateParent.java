b44411e2505d1f52c129a6f82d573fc0ef68b5bc
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-05-14 17:20:24 +0700

    Finished concepture question about Java 1401

---
+++ src/ctcInterview/java/J1401_PrivateParent.java
@@ -0,0 +1,28 @@
+package ctcInterview.java;
+
+/**
+ * This is a test to demonstrate the concept meantioned in PrivateConstructor.java
+ * 
+ * @author minhhoang
+ *
+ */
+public class J1401_PrivateParent {
+	
+	public J1401_PrivateParent() {
+		
+		// Parent cannot access too
+		J1401_PrivateConstructor pc = new J1401_PrivateConstructor();
+		
+		
+	}
+	
+	class SubParent {
+
+		public SubParent() {
+			// Sub class of parent cannot also access
+			J1401_PrivateConstructor pc = new J1401_PrivateConstructor();
+		}
+		
+	}
+
+}
