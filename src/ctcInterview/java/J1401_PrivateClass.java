d25f08b6fc99489ba5cb45e9d70ad91a1c7b64b6
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-05-15 21:55:59 +0700

    Ignored all helper class for private constructor question

--- src/ctcInterview/java/J1401_PrivateClass.java
+++ src/ctcInterview/java/J1401_PrivateClass.java
@@ -1,19 +1,19 @@
-package ctcInterview.java;
-
-/**
- * This is a test to demonstrate the concept meantioned in PrivateConstructor.java
- * 
- * @author minhhoang
- *
- */
-public class J1401_PrivateClass {
-	
-	public J1401_PrivateClass() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public void canItAccess() {
-		J1401_PrivateConstructor c = new J1401_PrivateConstructor(); // Because it is private it cannot access the class
-	}
-
-}
+//package ctcInterview.java;
+//
+///**
+// * This is a test to demonstrate the concept meantioned in PrivateConstructor.java
+// * 
+// * @author minhhoang
+// *
+// */
+//public class J1401_PrivateClass {
+//	
+//	public J1401_PrivateClass() {
+//		// TODO Auto-generated constructor stub
+//	}
+//	
+//	public void canItAccess() {
+//		J1401_PrivateConstructor c = new J1401_PrivateConstructor(); // Because it is private it cannot access the class
+//	}
+//
+//}
