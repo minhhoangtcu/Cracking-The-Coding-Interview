a88ab0fc3edd06a012a3f5f6b50d639d5530961c
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-05-18 11:26:20 +0700

    Finished counting occurances concepturally

---
+++ src/ctcInterview/moderate/M1709_FrequencyWord.java
@@ -0,0 +1,16 @@
+package ctcInterview.moderate;
+
+/**
+ * Design a method to find the frequency of occurences of any given word in a book.
+ * 
+ * @author minhhoang
+ *
+ */
+public class M1709_FrequencyWord {
+	
+	/**
+	 * If one time, just go through all for O(n)
+	 * If multiple times, go through all and build a Hashtable
+	 */
+
+}
