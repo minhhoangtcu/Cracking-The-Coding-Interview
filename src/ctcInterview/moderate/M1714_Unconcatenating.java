fc7ac8717c8551786284dee1583e28f2b3ecb6e7
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-07-14 14:35:27 -0500

    Added prompt to unconcatenating problem

--- src/ctcInterview/moderate/M1714_Unconcatenating.java
+++ src/ctcInterview/moderate/M1714_Unconcatenating.java
@@ -1,7 +1,23 @@
 package ctcInterview.moderate;
 
 /**
+ * You have just completed a lengthy document when you have an unfortunate
+ * Find/Replace mishap. You have accidentally removed all spaces, punctuation,
+ * and capitalization in the document. A sentence like
+ * "I reset the computer. It still didn't boot!" would become
+ * "iresetthecomputeritstilldidntboot." You figure that you can add back in the
+ * punctation and capitalization later, once you get the individual words
+ * properly separated. Most of the words will be in a dictionary, but some
+ * strings, like proper names, will not.
  * 
+ * Given a dictionary, design an algorithm to find the optimal way of
+ * unconcatenating a sequence of words. In this case, "optimal" is defined to be
+ * the parsing which minimizes the number of unrecognized sequences of
+ * characters.
+ * 
+ * For example, the string "jesslookedjustliketimberbrother" would be optimally
+ * parsed as "JESS looked just like TIM her brother". This parsing has seven
+ * unrecognized characters, which we have capitalized for clarity.
  * 
  * @author minhhoang
  *
