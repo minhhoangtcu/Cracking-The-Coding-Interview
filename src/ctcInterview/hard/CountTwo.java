99dd6f8f7dc8998ec9c2045c96eb8b06e020a13b
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-07-15 16:35:04 -0500

    Finished dynamic programming way (but it is way slower...)

--- src/ctcInterview/hard/CountTwo.java
+++ src/ctcInterview/hard/CountTwo.java
@@ -1,5 +1,9 @@
 package ctcInterview.hard;
 
+import java.util.Hashtable;
+
+import org.apache.commons.lang3.time.StopWatch;
+
 /**
  * Write a method to count the number of 2s that appear in all the numbers
  * between 0 and n (inclusive).
@@ -15,15 +19,70 @@ package ctcInterview.hard;
  *
  */
 public class CountTwo {
-	
+
 	public static void main(String[] args) {
+
+		// Test accuracy
+//		System.out.println(countBruteForce(2));
+//		System.out.println(countBruteForce(10));
+//		System.out.println(countBruteForce(12));
+//		System.out.println(countBruteForce(20));
+//		System.out.println(countBruteForce(30));
+//		System.out.println(countBruteForce(500));
+//		System.out.println(countBruteForce(5000));
+//		
+//		System.out.println(countDynamic(2));
+//		System.out.println(countDynamic(10));
+//		System.out.println(countDynamic(12));
+//		System.out.println(countDynamic(20));
+//		System.out.println(countDynamic(30));
+//		System.out.println(countDynamic(500));
+//		System.out.println(countDynamic(5000));
 		
-		System.out.println(countBruteForce(2));
-		System.out.println(countBruteForce(10));
-		System.out.println(countBruteForce(12));
-		System.out.println(countBruteForce(20));
-		System.out.println(countBruteForce(30));
+		// Test speed
+		StopWatch sw = new StopWatch();
+		int n = 1000000;
 		
+		sw.start();
+		System.out.println(countBruteForce(n));
+		sw.stop();
+		System.out.println("Brute Force Consumed (ns): \t\t" + sw.getNanoTime());
+		
+		sw.reset();
+		
+		sw.start();
+		System.out.println(countDynamic(n));
+		sw.stop();
+		System.out.println("Recursion/Dynamic Consumed (ns): \t" + sw.getNanoTime());
+
+	}
+
+	public static int countDynamic(int n) {
+		int count = 0;
+		Hashtable<Integer, Integer> numOf2s = new Hashtable<>();
+		for (int i = n; i >= 0; i--) {
+			count += countDynamicHelper(numOf2s, i);
+		}
+		return count;
+	}
+
+	public static int countDynamicHelper(Hashtable<Integer, Integer> numOf2s, int n) {
+
+		if (n == 0) {
+			return 0;
+		} else if (numOf2s.containsKey(n)) {
+			return numOf2s.get(n);
+		}
+
+		int count = 0;
+		if (n % 10 == 2) {
+			count = 1 + countDynamicHelper(numOf2s, n / 10);
+		} else {
+			count = countDynamicHelper(numOf2s, n / 10);
+		}
+		numOf2s.put(n, count);
+		return count;
+
 	}
 
 	/**
@@ -37,20 +96,20 @@ public class CountTwo {
 	 * @return
 	 */
 	public static int countBruteForce(int n) {
-		
+
 		int count = 0;
-		
+
 		for (int i = 0; i <= n; i++) {
-			
+
 			int a = i;
 			while (a != 0) {
 				if (a % 10 == 2)
 					count++;
 				a /= 10;
 			}
-			
+
 		}
-		
+
 		return count;
 	}
 
