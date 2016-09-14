a287f90e136a6da748cf3b6976398c98e06bd8f3
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-09-14 00:36:43 -0500

    Implemented much easier algorithm for drawing circle.

--- src/hackerrank/contest/misc/Lighthouse.java
+++ src/hackerrank/contest/misc/Lighthouse.java
@@ -6,53 +6,35 @@ public class Lighthouse {
 
 		Lighthouse lh = new Lighthouse();
 
-		lh.drawCircle(9, 4, 4, 2);
+		lh.drawCircle(9, 4, 4, 3);
 
 	}
 
 	public void drawCircle(int n, int cX, int cY, int r) {
 
-		int x = 0;
-		int y = r;
-		int d = 3 - 2 * r;
-
-		Drawer drawer = new Drawer(n, r, cX, cY);
-		while (x < y) {
-			drawCircle(drawer, r, cX, cY, x, y);
-			x++;
-			if (d < 0) {
-				d = d + 4 * x + 6;
-			} else {
-				y--;
-				d = d + 4 * (x - y) + 10;
+		Drawer d = new Drawer(n);
+		
+		for (int x = 0; x < n; x++) {
+			for (int y = 0; y < n; y++) {
+				if (isWithin(r, cX, cY, x, y))
+					d.addPoint(new Point(x, y));
 			}
-			drawCircle(drawer, r, cX, cY, x, y);
 		}
-		drawer.draw();
+		
+		d.draw();
+		
 	}
-
-	public void drawCircle(Drawer d, int r, int cX, int cY, int x, int y) {
-		d.addPoint(new Point(cX + x, cY + y));
-		d.addPoint(new Point(cX - x, cY + y));
-		d.addPoint(new Point(cX + x, cY - y));
-		d.addPoint(new Point(cX - x, cY - y));
-		d.addPoint(new Point(cX + y, cY + x));
-		d.addPoint(new Point(cX - y, cY + x));
-		d.addPoint(new Point(cX + y, cY - x));
-		d.addPoint(new Point(cX - y, cY - x));
-	}
-
 	
+	public static boolean isWithin(int r, int cX, int cY, int x, int y) {
+		return Math.pow(cX-x, 2) + Math.pow(cY-y, 2) <= r * r;
+	}
 
 	class Drawer {
 		boolean[][] board;
 		int n, r, cX, cY;
 
-		public Drawer(int n, int r, int cX, int cY) {
+		public Drawer(int n) {
 			this.n = n;
-//			this.r = r; 
-//			this.cX = cX;
-//			this.cY = cY;
 			board = new boolean[n][n];
 		}
 
@@ -61,10 +43,6 @@ public class Lighthouse {
 				board[p.x][p.y] = true;
 		}
 		
-//		public boolean isWithin(int r, int x, int y) {
-//			return Math.pow(cX-x, 2) + Math.pow(cY-y, 2) <= r * r;
-//		}
-
 		public void draw() {
 			for (int x = 0; x < board.length; x++) {
 				for (int y = 0; y < board.length; y++) {
