afce8a314de68c043c1cbf28d8a0d4791b43878d
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-12-30 20:49:54 -0800

    Solved EPI problem

---
+++ src/epi/linkedLists/LL84_OverlappingCycleFree.java
@@ -0,0 +1,34 @@
+package epi.linkedLists;
+
+/**
+ * Given two singly linked lists there may be list nodes that are common to
+ * both. (This may not be a bug - it may be desirable from the perspective of
+ * reducing memory footprint, as in the flyweight pattern, or maintaining a
+ * canonical form.)
+ * 
+ * Write a program that takes two cycle-free singly linked lists, and determines
+ * if there exists a node that is common to both lists.
+ */
+public class LL84_OverlappingCycleFree {
+	
+	public boolean isConverge(Node rootA, Node rootB) {
+		return getLastNode(rootA) == getLastNode(rootB);
+	}
+	
+	public Node getLastNode(Node root) {
+		
+		Node runner = root;
+		
+		while (runner != null && runner.next != null) {
+			runner = runner.next;
+		}
+		
+		return runner;
+	}
+
+}
+
+class Node {
+	Node next;
+	int id;
+}
