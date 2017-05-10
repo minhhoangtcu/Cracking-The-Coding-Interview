1b4dc1cbd4fe53217f2143caa9c177983c535499
Minh Hoang <minh.hoang@tcu.edu>
2017-05-09 20:34:37 -0500

    get subset size k in set size n

---
+++ src/epi/recursion/R165_SubsetK.java
@@ -0,0 +1,50 @@
+package epi.recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Write a program which computes all size k subsets of {1, 2, ..., n}, where k and n are program
+ * inputs. For example, if k = 2 and n = 5, then the result is the following: {{1, 2}, {1, 3}, etc.}
+ * 
+ * @author minhhoang
+ *
+ */
+public class R165_SubsetK {
+  
+  public static void main(String[] args) {
+    ListUtils.printListofList(getSubsetsK(Arrays.asList(1, 2, 3, 4, 5), 3));
+  }
+
+  public static List<List<Integer>> getSubsetsK(List<Integer> baseset, int k) {
+    List<List<Integer>> subsets = new LinkedList<>();
+    
+    addSubset(0, 0, k, new LinkedList<>(), baseset, subsets);
+    
+    return subsets;
+  }
+  
+  /**
+   * 
+   * 
+   * @param buildingIndex
+   * @param gettingIndex
+   */
+  private static void addSubset(int buildingIndex, int gettingIndex, int k, List<Integer> set, 
+      List<Integer> baseset, List<List<Integer>> subsets) {
+    if (buildingIndex == k) {
+      subsets.add(new ArrayList<>(set));
+      return;
+    } else if (gettingIndex == baseset.size()) {
+      return; // if not in correct size and out of element to build
+    }
+      
+    for (int b = gettingIndex; b < baseset.size(); b++) {
+      set.add(baseset.get(b));
+      addSubset(buildingIndex + 1, b + 1, k, set, baseset, subsets);
+      set.remove(set.size() - 1); // remove last added
+    }
+  }
+}
